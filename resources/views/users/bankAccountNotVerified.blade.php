f9423ad4cf20a6ff4d021867ab55a3fa04f8b12b
Daniel Arnalis <d.arnalis@digitappgency.com>
2017-02-22 11:29:51 +0100

    Alignement au centre du bouton de validation du mandat

--- resources/views/users/bankAccountNotVerified.blade.php
+++ resources/views/users/bankAccountNotVerified.blade.php
@@ -23,7 +23,7 @@ active
                     <!-- Call Action Text -->
                     <!-- <h2 class="primary">Pour vérifier vos coordonnés bancaires et en valider l’utilisation pour recevoir ou émettre des remboursements, vous devez cliquer sur le lien de validation qui vient de vous être envoyé par email.</h2> -->
                     <h2 class="primary">Pour valider vos coordonnés bancaires, vous devez impérativement <strong>cliquez</strong> sur le bouton "valider" dans les <strong>59 prochaines minutes</strong>.</h2>
-                    @if(session('url'))<p><a href="{{ session('url') }}" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-btn fa-check"></i> Valider</a></p>@endif
+                    @if(session('url'))<p align="center"><a href="{{ session('url') }}" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-btn fa-check"></i> Valider</a></p>@endif
                      <!-- Call Action Button -->
               </div>
           </div>
